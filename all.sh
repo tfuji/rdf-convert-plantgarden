6ae38b17d91df331444bf2bebd981e74228aaa41
Takatomo Fujisawa <fujisawa.takatomo@gmail.com>
2021-08-10 15:14:25 +0900

    Fixed bugs.

--- all.sh
+++ all.sh
@@ -8,11 +8,11 @@ perl scripts/tsv2ttl_subspecies.pl $PGARDEN_IN/mysql/import_species/import_sub_s
 echo "convert genome..."
 perl scripts/tsv2ttl_genomes.pl $PGARDEN_IN/mysql/import_genome/import_genome_20210525.tsv > $PGARDEN_RDF/rdf/pg_genome.ttl
 echo "convert gene..."
-perl scripts/tsv2ttl_gene.pl   /home/metabobank/private/PlantGarden/data/mysql/import_gene/import_data/*_gene.import.tsv > $PGARDEN_RDF/rdf/pg_gene.ttl
+perl scripts/tsv2ttl_gene.pl   $PGARDEN_IN/mysql/import_gene/import_data/*_gene.import.tsv > $PGARDEN_RDF/rdf/pg_gene.ttl
 echo "convert marker..."
-perl scripts/tsv2ttl_marker.pl /home/metabobank/private/PlantGarden/data/mysql/import_marker/*.import_marker.tsv > $PGARDEN_RDF/rdf/pg_marker.ttl
+perl scripts/tsv2ttl_marker.pl $PGARDEN_IN/mysql/import_marker/*.import_marker.tsv > $PGARDEN_RDF/rdf/pg_marker.ttl
 echo "convert trait..."
-perl scripts/tsv2ttl_trait.pl /home/metabobank/private/PlantGarden/data/mysql/import_trait/import_trait_related_*.txt > $PGARDEN_RDF/rdf/pg_trait.ttl
+perl scripts/tsv2ttl_trait.pl $PGARDEN_IN/mysql/import_trait/import_trait_related_*.txt > $PGARDEN_RDF/rdf/pg_trait.ttl
 echo "wget pubtator..."
 wget http://togodb.org/release/pg_pubtator.ttl -P $PGARDEN_RDF/rdf/
 echo "wget rgaugury..."
